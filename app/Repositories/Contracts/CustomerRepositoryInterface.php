501abeb717adc29d3f36f3328c0fdfac4d76b195
ngoctusoftware <ngoctusoftware@gmail.com>
2026-09-07 09:13:02 +0700

    Fix

--- app/Repositories/Contracts/CustomerRepositoryInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Repositories\Contracts;
-
-interface CustomerRepositoryInterface extends RepositoryInterface
-{
-    public function findByEmail(string $email): ?object;
-}
--- public/js/app.js
+++
@@ -1,4 +0,0 @@
-// Placeholder cho JS tương tác phía client (đếm số lượng giỏ hàng động qua AJAX, v.v.)
-document.addEventListener('DOMContentLoaded', function () {
-    console.log('ZEK Shop frontend loaded');
-});
